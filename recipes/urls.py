2c765e44f4c52784bcd7fc54e3790757e3e3dd19
Reiner Santiago <reiner.santyago@gmail.com>
2026-06-25 18:08:45 -0300

    Removendo pasta

--- recipes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from recipes.views import cliente, home, my_view
-
-urlpatterns = [
-    path('', home),
-    path('sobre/', my_view),
-    path('cliente/', cliente),
-]
